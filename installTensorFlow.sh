f2e9aab9dc55b56f38417a80b625bb072ca5ba11
analgotrader <oneangrytrader@gmail.com>
2022-03-14 05:06:41 +0100

    .

---
+++ installTensorFlow.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILENAME=libtensorflow-cpu-linux-x86_64-2.7.0.tar.gz
+wget -q --no-check-certificate https://storage.googleapis.com/tensorflow/libtensorflow/${FILENAME}
+sudo tar -C /usr/local -xzf ${FILENAME}
+
+sudo ldconfig /usr/local/lib
